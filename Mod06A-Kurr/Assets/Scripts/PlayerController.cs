3804927b08be538f86d1d5ffb32a8a3b644e5e6d
Ku-Bri <106645400+Ku-Bri@users.noreply.github.com>
2023-03-09 13:21:31 -0600

    Played with objects and colliders

--- Mod06A-Kurr/Assets/Scripts/PlayerController.cs
+++ Mod06A-Kurr/Assets/Scripts/PlayerController.cs
@@ -6,8 +6,8 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
 
-    public float speed = 5.0f;
-    public float turnSpeed;
+    public float speed = 20.0f;
+    public float turnSpeed = 45.0f;
     public float horizontalInput;
     public float forwardInput;
     
@@ -25,6 +25,6 @@ public class PlayerController : MonoBehaviour
         horizontalInput = Input.GetAxis("Horizontal");
         forwardInput = Input.GetAxis("Vertical");
         transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
-        transform.Translate(Vector3.right * Time.deltaTime * horizontalInput);
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
     }
 }
